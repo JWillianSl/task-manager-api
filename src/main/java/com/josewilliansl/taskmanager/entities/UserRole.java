dd3089469d42e51575a5aca71fbadefbe382ac29
JWillianSl <jose.williansl@hotmail.com>
2024-11-09 14:56:45 -0300

    feat: adiciona configurações iniciais do banco de dados ao properties

--- src/main/java/com/josewilliansl/taskmanager/entities/UserRole.java
+++ src/main/java/com/josewilliansl/taskmanager/entities/UserRole.java
@@ -8,12 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "user_roles")
 public class UserRole {
 
     @Id
@@ -21,6 +23,7 @@ public class UserRole {
     @Column(name = "role_id")
     private Long id;
 
+    @Column(name = "role_name")
     private String name;
 
     @ManyToMany(mappedBy = "roles")
---
+++ src/main/resources/idata.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO user_roles (role_id, role_name) VALUES (1, "ADMIN");
+INSERT IGNORE INTO user_roles (role_id, role_name) VALUES (2, "COMMON");
+INSERT IGNORE INTO user_roles (role_id, role_name) VALUES (3, "MANAGER");
+INSERT IGNORE INTO user_roles (role_id, role_name) VALUES (4, "MEMBER");
+INSERT IGNORE INTO user_roles (role_id, role_name) VALUES (5, "LEADER");

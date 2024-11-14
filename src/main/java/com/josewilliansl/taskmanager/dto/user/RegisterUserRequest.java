8e4dc5a7dd0fa4b764c485d64f3f8a744fee37c2
JWillianSl <jose.williansl@hotmail.com>
2024-11-14 15:22:37 -0300

    refactor: remove anotação perdida no dto de registro do usuário

--- src/main/java/com/josewilliansl/taskmanager/dto/user/RegisterUserRequest.java
+++ src/main/java/com/josewilliansl/taskmanager/dto/user/RegisterUserRequest.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-@NotBlank()
 public record RegisterUserRequest(@NotBlank(message = "Full name is not defined.") String fullName,
         @NotBlank(message = "Nick name is not defined.") String nickName,
         @NotBlank(message = "Date of birth is not defined.") LocalDate dateOfBirth,

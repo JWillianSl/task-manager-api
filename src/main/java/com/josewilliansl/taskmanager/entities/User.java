5f09711380d713bf7c65cbced2a7e27e5582eb3d
JWillianSl <jose.williansl@hotmail.com>
2024-11-14 22:42:22 -0300

    feat: adiciona implementação básica para a funcionalidade de registro

--- src/main/java/com/josewilliansl/taskmanager/entities/User.java
+++ src/main/java/com/josewilliansl/taskmanager/entities/User.java
@@ -19,10 +19,12 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tb_user")
--- src/main/java/com/josewilliansl/taskmanager/service/user/UserAccessService.java
+++ src/main/java/com/josewilliansl/taskmanager/service/user/UserAccessService.java
@@ -1,9 +1,13 @@
 package com.josewilliansl.taskmanager.service.user;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
 import com.josewilliansl.taskmanager.dto.user.RegisterUserRequest;
 import com.josewilliansl.taskmanager.dto.user.UserResponse;
+import com.josewilliansl.taskmanager.entities.Role;
+import com.josewilliansl.taskmanager.entities.User;
 import com.josewilliansl.taskmanager.repository.RoleRepository;
 import com.josewilliansl.taskmanager.repository.UserRepository;
 
@@ -11,15 +15,23 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class UserAccessService implements UserAccess{
-    
+public class UserAccessService implements UserAccess {
+
     private UserRepository userRepository;
 
     private RoleRepository roleRepository;
 
     @Override
     public UserResponse register(RegisterUserRequest registerUserData) {
-        return null;
+        Role basicRole = roleRepository.findByName("COMMON");
+
+        User newUser = User.builder().fullName(registerUserData.fullName()).username(registerUserData.nickName())
+                .dateOfBirth(null).email(registerUserData.email()).password(registerUserData.password())
+                .roles(Set.of(basicRole)).build();
+        
+        userRepository.save(newUser);
+
+        return new UserResponse(newUser.getFullName(), newUser.getUsername(), newUser.getEmail());
     }
 
 }
